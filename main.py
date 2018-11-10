7ea9b60ae0c71db36902e4e666a695a0b95b11ee
coleyoungmusic <coleyoungmusic@gmail.com>
2018-11-10 12:28:27 -0600

    changes made

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from pydub import AudioSegment
-from appJar import appjar
+from appJar import gui
 
-app = gui('Audio Converter')
+def convert(btn):
+    if app.getRadioButton("choice") == "MP3":
+        print("MP3")
+    else:
+        print("WAV")
 
+# set
+app = gui('Audio Converter', useTtk=True)
+app.setTtkTheme('clam')
 
+app.addFileEntry("audio_file")  #  select audio file
+
+app.addRadioButton("choice", "MP3")
+app.addRadioButton("choice", "WAV")
+
+app.addButton("CONVERT", convert)
 
 app.go()
