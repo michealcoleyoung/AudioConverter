190aa72585098e57f0a99cd34bab5429df79af6a
coleyoungmusic <coleyoungmusic@gmail.com>
2018-11-15 22:08:57 -0600

    instructions added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from appJar import gui
 import os
 
 # TODO: Make sure that a message box appears to confirm successful conversion
+# TODO: Once file is converted show it as the actual file name and not test.mp3 or test.wav etc.
 
 
 def convert(btn):
@@ -10,10 +11,14 @@ def convert(btn):
     song = AudioSegment.from_file(audio_file) #  Identifies what file type (WAV or MP3)
 
     if app.getRadioButton("choice") == "MP3":
+        song.export('/Users/michealcoleyoung/Downloads/test.mp3', format="mp3")
         #  Convert file to MP3 and save to the downloads directory
 
     else:
+        song.export('/Users/michealcoleyoung/Downloads/test.wav', format="wav")
         #  Convert file to WAV and save to the downloads directory
+
+    #  Message box will go here
 
 
 app = gui('Audio Converter', useTtk=True)
