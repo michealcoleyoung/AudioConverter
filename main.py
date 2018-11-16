c913516cc4f2bab22bd6900989cbc176f0990551
coleyoungmusic <coleyoungmusic@gmail.com>
2018-11-15 22:13:29 -0600

    testing pycharm git commit and push

--- main.py
+++ main.py
@@ -7,26 +7,26 @@ import os
 
 
 def convert(btn):
-    audio_file = app.getEntry("audio_file")  #  Selects audio from filepath
-    song = AudioSegment.from_file(audio_file) #  Identifies what file type (WAV or MP3)
+    audio_file = app.getEntry("audio_file")    # Selects audio from filepath
+    song = AudioSegment.from_file(audio_file)  # Identifies what file type (WAV or MP3)
 
     if app.getRadioButton("choice") == "MP3":
         song.export('/Users/michealcoleyoung/Downloads/test.mp3', format="mp3")
-        #  Convert file to MP3 and save to the downloads directory
+        # Convert file to MP3 and save to the downloads directory
 
     else:
         song.export('/Users/michealcoleyoung/Downloads/test.wav', format="wav")
-        #  Convert file to WAV and save to the downloads directory
+        # Convert file to WAV and save to the downloads directory
 
-    #  Message box will go here.
+    # Message box will go here.
 
 
 app = gui('Audio Converter', useTtk=True)
 app.setTtkTheme('clam')
 
-app.addFileEntry("audio_file")  #  select audio file
+app.addFileEntry("audio_file")  # select audio file
 
-#  audio formats to choose from are WAV and MP3
+# audio formats to choose from are WAV and MP3
 app.addRadioButton("choice", "MP3")
 app.addRadioButton("choice", "WAV")
 
