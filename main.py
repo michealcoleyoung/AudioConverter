d080bb7b04519185e3c7eb7ffa30d0655862a8fe
coleyoungmusic <coleyoungmusic@gmail.com>
2018-11-18 00:11:51 -0600

    project updated

--- main.py
+++ main.py
@@ -1,38 +1,23 @@
 from pydub import AudioSegment
 from appJar import gui
-import os
-
-# TODO: Make sure that a message box appears to confirm successful conversion
-# TODO: Once file is converted show it as the actual file name and not test.mp3 or test.wav etc.
+import ntpath
 
 
 def convert(btn):
     audio_file = app.getEntry("audio_file")    # Selects audio from filepath
     song = AudioSegment.from_file(audio_file)  # Identifies what file type (WAV or MP3 etc.)
-    file_name = app.getEntry("file_name")      # For testing purposes
+    file_name = ntpath.basename(audio_file)    # Gets basename from file path (a/b/c/basename)
 
-    if app.getRadioButton("choice") == "MP3":
-        song.export(f'/Users/michealcoleyoung/Downloads/{file_name}.mp3', format="mp3")
-        # Convert file to MP3 and save to the downloads directory
-
-    else:
-        song.export(f'/Users/michealcoleyoung/Downloads/{file_name}.wav', format="wav")
-        # Convert file to WAV and save to the downloads directory
+    song.export(f'/Users/michealcoleyoung/Downloads/{file_name.replace(".wav", "")}.mp3', format="mp3")
+    # Convert file to MP3 and save to the downloads directory
 
     app.infoBox("message", "Song was successfully converted!")
 
 
-app = gui('Audio Converter', useTtk=True)
+app = gui('WAV-P3', useTtk=True)
 app.setTtkTheme('clam')
 
 app.addFileEntry("audio_file")  # select audio file
-
-# audio formats to choose from are WAV and MP3
-app.addRadioButton("choice", "MP3")
-app.addRadioButton("choice", "WAV")
-app.addEntry("file_name")
-app.setEntryDefault("file_name", "Enter the name of the file you are converting")
-
 app.addButton("CONVERT", convert)
 
 app.go()
